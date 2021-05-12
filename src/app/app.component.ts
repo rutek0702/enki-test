8687fa4cf2893fd10496cd0429da2ffb692ca9f4
k.rutkowski0702@gmail.com <Rtk_01116>
2021-05-12 17:13:04 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ComponentFactoryResolver, Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import {FirstStepComponent} from './components/first-step/first-step.component';
+import {SecondStepComponent} from './components/second-step/second-step.component';
+import {ThirdStepComponent} from './components/third-step/third-step.component';
+import {ChangeDetection} from '@angular/cli/lib/config/schema';
+import {BehaviorSubject} from 'rxjs';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'enki';
+export class AppComponent implements AfterViewInit {
+  steps = 'FirstStepComponent';
+
+
+  @ViewChild('container', {read: ViewContainerRef}) private container: ViewContainerRef;
+
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+  }
+
+  ngAfterViewInit(): void {
+    this.loadSteps();
+  }
+
+  loadSteps() {
+    const containerRef = this.container;
+    containerRef.clear();
+    const componentRef = containerRef.createComponent(
+      this.componentFactoryResolver.resolveComponentFactory(this.getTemplateName(this.steps))
+    );
+
+    componentRef.instance.changeStep.subscribe(val => {
+      this.steps = val;
+      this.loadSteps();
+    });
+  }
+
+  getTemplateName(parm) {
+    switch (parm) {
+      case 'FirstStepComponent':
+        return FirstStepComponent;
+      case 'SecondStepComponent':
+        return SecondStepComponent;
+      case 'ThirdStepComponent':
+        return ThirdStepComponent;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FirstStepComponent} from './components/first-step/first-step.component';
+import {SecondStepComponent} from './components/second-step/second-step.component';
+import {ThirdStepComponent} from './components/third-step/third-step.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstStepComponent,
+    SecondStepComponent,
+    ThirdStepComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
